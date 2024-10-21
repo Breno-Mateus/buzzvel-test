17fbb4a80e56dbfe55616c1e3e64908b22cd8689
Breno-Mateus <brenomateus36@gmail.com>
2024-10-21 17:17:39 -0300

    feat: add section5

---
+++ src/components/section5/index.tsx
@@ -0,0 +1,9 @@
+const Section5 = () => {
+    return (
+        <section className="h-[1046px] w-full bg-colorButton">
+
+        </section>
+    )
+}
+
+export default Section5
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -3,6 +3,7 @@ import Section1 from "../../components/section1"
 import Section2 from "../../components/section2"
 import Section3 from "../../components/section3"
 import Section4 from "../../components/section4"
+import Section5 from "../../components/section5"
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             <Section2 />
             <Section3 />
             <Section4 />
+            <Section5 />
         </div>
     )
 }
