926069623b44b5516751612a47d655ccd651c38b
Breno-Mateus <brenomateus36@gmail.com>
2024-10-21 08:44:26 -0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,31 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="text-textColor flex gap-38">
+                <div>
+                    <h3 className="text-32px leading-35.2px font-bold">soller</h3>
+                </div>
+
+                <div className="flex gap-4 text-base font-medium tracking-0.5px">
+                    <a>Products</a>
+                    <a>Solutions</a>
+                    <a>Services</a>
+                    <a>Configure</a>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src={}/>
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        colorPrimary: '#ffff',
+        textColor: '#0F172A',
+        textColorSecondary: '#0369A1',
+        colorButton: '#581C87',
+      },
+      gap : {
+        '38': '38px',
+      },
+      fontSize: {
+        '32px': '32px',
+      },
+      lineHeight: {
+        '35.2px': '35.2px',
+      },
+      letterSpacing: {
+        '0.5px': '0.5px',
+      },
+    },
+  },
+  plugins: [],
+}
+
