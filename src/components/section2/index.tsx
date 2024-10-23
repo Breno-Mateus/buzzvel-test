7f9266c0132643d02d4e1f80079dd0549e298d80
Breno-Mateus <brenomateus36@gmail.com>
2024-10-23 10:34:28 -0300

    feat: add section2-mobile-first

--- src/components/section2/index.tsx
+++ src/components/section2/index.tsx
@@ -1,21 +1,22 @@
-import Desktop from "../../assets/Desktop.png"
-import Elipse1 from "../../assets/geometric figures/figure2.png"
-import Elipse2 from "../../assets/geometric figures/figure3.png"
+import Desktop from "../../assets/mobile/Desktop.png"
+import Elipse1 from "../../assets/mobile/geometric-figures-mobile/Ellipse 14.png"
+import Elipse2 from "../../assets/mobile/geometric-figures-mobile/Ellipse 15.png"
 
 const Section2 = () => {
     return (
-        <section className="relative flex flex-col items-center h-[1282px] text-textColor text-center gap-20 p-20 w-full">
-            <div className="w-[900px] h-[188px] flex flex-col gap-6">
-                <p className="text-textColorOther text-xl leading-[22px] font-medium">No more waste</p>
-                <h1 className="text-[56px] leading-[61.6px] font-extrabold">Pick the Sun</h1>
-                <p className="text-[20px] leading-[36px] font-medium">Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.</p>
-            </div>
-
-            <div className="absolute mt-[110px] w-full h-[800px] -z-10 flex">
+        <section className="flex flex-col items-center text-textColor text-center gap-16 px-4 py-12 w-full h-[604px]">
+            
+            <div className="absolute w-full mt-[151px] -z-10 flex">
                 <img src={Elipse1} className="absolute top-0 left-0"
                 />
-                <img src={Elipse2} className="absolute bottom-0 right-0"
+                <img src={Elipse2} className="absolute right-0 top-20"
                 />
+            </div>
+
+            <div className="flex flex-col gap-6">
+                <p className="text-textColorOther text-[16px] leading-[17.6px] font-medium">No more waste</p>
+                <h1 className="text-[32px] leading-[35.2px] font-bold">Pick the Sun</h1>
+                <p className="text-[16px] leading-[22.4px] font-normal">Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.</p>
             </div>
 
             <div>
