87597b64c405431ddfc6a284f4bb5b490729f2f7
Breno-Mateus <brenomateus36@gmail.com>
2024-10-23 11:25:20 -0300

    feat: add section4-mobile-first

--- src/components/section4/index.tsx
+++ src/components/section4/index.tsx
@@ -1,33 +1,29 @@
 import BoxText from "../box-text"
-import Figura5 from "../../assets/geometric figures/figure5.png"
-import Figura6 from "../../assets/geometric figures/figure6.png"
-import Mobile from "../../assets/Mobile-App-Placeholder-2.png"
+import Figura5 from "../../assets/mobile/geometric-figures-mobile/Rectangle 17.png"
+import Mobile from "../../assets/mobile/Phone1.png"
 
 const Section4 = () => {
     return (
-        <section className="h-[1060px] w-full p-20 flex gap-40 relative">
-            <div className="w-[1180px] h-[610px] flex flex-col gap-12 text-textColor">
-                <div className="flex flex-col h-[152px] gap-6">
-                    <p className="text-textColorOther text-xl leading-[22px] font-medium">System features</p>
-                    <h1 className="text-[56px] leading-[61.6px] font-extrabold">Powerful features</h1>
-                    <p className="text-[20px] leading-[36px] font-medium">Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis semper id sit ornare maecenas lectus sed.</p>
-                </div>
+        <section className="w-full px-4 py-12 gap-16 flex flex-col relative">
 
-                <BoxText title1="Erat sit" text1="Id quis lectus pharetra, ultricies integer montes, amet, gravida consectetur. Nunc convallis fringilla nisl magna sagittis." title2="Ullamcorper arcu" text2="Ipsum at id hendrerit amet faucibus commodo quam nullam. Lectus auctor habitant duis dictum."/>
-
-                <BoxText title1="Et pellentesque" text1="Mi vitae, massa eu molestie massa euismod volutpat condimentum. Blandit molestie ullamcorper hendrerit purus lorem vulputate." title2="Amet egestas" text2="Elementum, libero, lacus, vel ullamcorper at velit id. Malesuada velit et, ullamcorper malesuada amet, felis."/>               
+            <div className="flex flex-col h-[154px] gap-6 text-center text-textColor">
+                <p className="text-textColorOther text-[16px] leading-[17.6px] font-medium">Services</p>
+                <h1 className="text-[32px] leading-[35.2px] font-bold">Personalized services</h1>
+                <p className="text-[16px] leading-[22.4px] font-normal">Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis semper id sit ornare maecenas lectus sed.</p>
             </div>
 
-            <div>
-                <img src={Mobile} />
-            </div>
+            <div className="gap-12 flex flex-col items-center justify-center text-center">
+                <BoxText title1="Et mauris" text1="Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu accumsan tristique." title2="Eget sit" text2="Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa."/>
 
-            <div className="absolute right-0 top-0 -z-10">
-                <img src={Figura5} />
-            </div>
+                <BoxText title1="Imperdiet pellentesque" text1="Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient tristique nulla." title2="Non libero" text2="Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros, consectetur viverra."/>
 
-            <div className="absolute right-[60px] top-[655px]">
-                <img src={Figura6} />
+                <div className="absolute right-0 t bottom-20 -z-10">
+                    <img src={Figura5}/>
+                </div>                
+            </div>
+            
+            <div className="flex items-center justify-center">
+                <img src={Mobile} />               
             </div>
         </section>
     )
