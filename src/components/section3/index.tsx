2a43628941d01e18ba1d17421d9f24388cbc302f
Breno-Mateus <brenomateus36@gmail.com>
2024-10-24 10:12:42 -0300

    feat: web section3 version

--- src/components/section3/index.tsx
+++ src/components/section3/index.tsx
@@ -1,28 +1,56 @@
 import Mobile from "../../assets/mobile/Phone.png"
 import Figura4 from "../../assets/mobile/geometric-figures-mobile/Rectangle 16.png"
 import BoxText from "../box-text"
+//figure web version
+import Figure from "../../assets/geometric figures/figura4.png"
+import web from "../../assets/Mobile-App-Placeholder-1.png"
 
 const Section3 = () => {
     return (
-        <section className="w-full px-4 py-12 gap-16 flex flex-col relative">
-
-            <div className="flex flex-col h-[154px] gap-6 text-center text-textColor">
-                <p className="text-textColorOther text-[16px] leading-[17.6px] font-medium">Services</p>
-                <h1 className="text-[32px] leading-[35.2px] font-bold">Personalized services</h1>
-                <p className="text-[16px] leading-[22.4px] font-normal">Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis semper id sit ornare maecenas lectus sed.</p>
+        <section className="
+        w-full px-4 py-12 gap-16 flex flex-col relative
+        md:flex-row md:gap-[160px] md:p-20">
+            
+            <div className="
+            hidden
+            md:block">
+                <img src={web}/>               
             </div>
 
-            <div className="gap-12 flex flex-col items-center justify-center text-center">
-                <BoxText title1="Et mauris" text1="Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu accumsan tristique." title2="Eget sit" text2="Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa."/>
+            <div className="md:flex md:flex-col">
 
-                <BoxText title1="Imperdiet pellentesque" text1="Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient tristique nulla." title2="Non libero" text2="Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros, consectetur viverra."/>
+            
+                <div className="
+                flex flex-col gap-6 text-center text-textColor
+                md:text-start">
+                    <p className="text-textColorOther text-[16px] leading-[17.6px] font-medium">Services</p>
+                    <h1 className="text-[32px] leading-[35.2px] font-bold">Personalized services</h1>
+                    <p className="text-[16px] leading-[22.4px] font-normal">Pretium lectus ultrices sit tempor, sit ullamcorper volutpat et et. Auctor turpis semper id sit ornare maecenas lectus sed.</p>
+                </div>
 
-                <div className="absolute left-0 bottom-20 -z-10">
-                    <img src={Figura4}/>
-                </div>                
+                <div className="
+                gap-12 flex flex-col items-center justify-center text-center
+                md:text-start">
+                    <BoxText title1="Et mauris" text1="Posuere quis sed mauris non curabitur pretium elementum eget. Feugiat sed maecenas eu accumsan tristique." title2="Eget sit" text2="Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa."/>
+
+                    <BoxText title1="Imperdiet pellentesque" text1="Platea arcu dapibus non magna cursus lectus id sollicitudin. Enim viverra parturient tristique nulla." title2="Non libero" text2="Congue mauris sem vel, urna viverra. Urna, nibh leo suscipit purus ut sed eros, consectetur viverra."/>
+
+                    <div className="
+                    absolute left-0 bottom-20 -z-10
+                    md:top-0">
+                        <img src={Figura4} className="
+                        md:hidden"/>
+                        <img src={Figure} className="
+                        hidden
+                        md:block"/>
+                    </div>                
+                </div>
+
             </div>
             
-            <div className="flex items-center justify-center">
+            <div className="
+            flex items-center justify-center
+            md:hidden">
                 <img src={Mobile} />               
             </div>
         </section>
