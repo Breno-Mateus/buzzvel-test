bc45f70398d68c34df2aadde3b6eaa65d855bb7b
Breno-Mateus <brenomateus36@gmail.com>
2024-10-24 08:22:06 -0300

    feat: [200~web header version

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,55 @@
+//Figure mobile
 import Figura1 from "../../assets/geometric figures/figure11.png"
 import Worker from "../../assets/Mask masked.png"
+//figure web
+import Fone from "../../assets/headset.png"
+import Figure from "../../assets/geometric figures/figure1.png"
+import workerWeb from "../../assets/work.png"
+//outhers
+import Button from "../button"
 
 const Header = () => {
     return (
-        <header className="pb-8 w-full h-[365px]">
-            <div className="text-textColor flex flex-col justify-between h-[56px] py-2 px-4">
+        <header className="
+        w-full h-[365px] pb-8 relative
+        md:px-20 md:py-6 md:flex md:items-center md:justify-between md:h-[112px]">
+
+            <div className="
+            text-textColor h-[56px] py-2 px-4
+            md:flex md:gap-[38px]">
                 
                 <div className="h-[36px] w-[81px]">
                     <h3 className="text-[32px] leading-[35.2px] font-bold">soller</h3>
                 </div>
 
-                <div>
-                    {/* add button */}
+                <div className="
+                hidden text-base leading-6 font-medium tracking-[0.5px]
+                md:flex md:gap-4">
+                    <a className="px-2 py-3">Products</a>
+                    <a className="px-2 py-3">Solutions</a>
+                    <a className="px-2 py-3">Services</a>
+                    <a className="px-2 py-3">Configure</a>
                 </div>
             </div>
 
-            <div className="w-[307.31px] h-[333px] ml-[67.69px]">
-                <img src={Figura1} className="w-[307.31px] h-[333px] absolute top-0 right-0 object-contain z-10"/>
-                <img src={Worker} className="w-[307.31px] h-[333px] absolute top-0 right-0 object-contain z-10"/>
+            <div className="
+            hidden
+            md:flex md:gap-4">
+                <div className="px-2 py-3 flex  items-center justify-center gap-4">
+                    <img src={Fone} className="h-6 w-6"/>
+                    <p className="text-textColorSecondary text-base leading-6 font-medium tracking-[0.5px]">555 818 282</p>
+                </div>
+                <Button borderColor="border-colorButton" backgroundHover="hover:bg-colorButton" textColor="text-colorButton" textColorHover="hover:text-colorButtonSecondary" arrow="src/assets/arrow-right-1.svg" arrowHover="src/assets/arrow-right-2.svg"/>
+            </div>
+
+            <div className="w-[307.31px] h-[333px] ml-[67.69px] -z-10 absolute top-0 right-0 md:hidden">
+                <img src={Figura1}/>
+                <img src={Worker} className="absolute top-0"/>
+            </div>
+
+            <div className="hidden md:block md:-z-10 md:absolute md:top-0 md:right-0">
+                <img src={Figure}/>
+                <img src={workerWeb} className="absolute top-0"/>
             </div>
         </header>
     )
